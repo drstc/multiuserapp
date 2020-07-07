75b0fa7658e7f1ac011fdc9b9dc6b6988db7540f
admin <admin@10.70.4.38>
2020-07-07 16:09:08 +0200

    Test

---
+++ multiuserapp/src/main/java/ch/zli/m223/dragana/multiuserapp/MultiuserappApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.dragana.multiuserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiuserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiuserappApplication.class, args);
+	}
+
+}
---
+++ multiuserapp/src/test/java/ch/zli/m223/dragana/multiuserapp/MultiuserappApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.dragana.multiuserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiuserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
